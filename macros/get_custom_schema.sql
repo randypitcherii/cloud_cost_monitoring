5a37cf912ac34787a8a91d15fd1c8923f5fff442
Randy Pitcher <randypitcherii@gmail.com>
2020-01-20 18:56:28 -0600

    cleanup, reformatting, addition of custom schema, and snowflake basic costs

---
+++ macros/get_custom_schema.sql
@@ -0,0 +1,11 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- set default_schema = target.schema -%}
+
+    {%- if custom_schema_name is none -%}
+        {{ default_schema }} 
+    {%- elif 'prod' == target.name -%}
+        {{ custom_schema_name | trim }}
+    {%- else -%}
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+    {%- endif -%}
+{%- endmacro %}
--- models/staging/aws/stg_hashmap_aws_costs.sql
+++ models/staging/aws/stg_hashmap_aws_costs.sql
@@ -1,7 +1,5 @@
 with raw_costs as (
-
   select * from {{ source('aws_costs', 'hashmap_aws_costs') }}
-
 )
 
 select * from raw_costs
